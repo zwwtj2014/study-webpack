cc47210eef7ce17e40cd4804d5c91176fa504b83
zwwtj2014 <529474551@qq.com>
2018-01-13 00:03:14 +0800

    测试缓存和vendor的hash变化的问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
-const ConsoleLogOnBuildWebpackPlugin = require("./plugins/consolelog-onbuildwebpack-plugin/ConsoleLogOnBuildWebpackPlugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const Webpack = require("webpack");
+const ConsoleLogOnBuildWebpackPlugin = require('./plugins/consolelog-onbuildwebpack-plugin/ConsoleLogOnBuildWebpackPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  // entry: {
+  //   a: './src/entry/a.js',
+  //   b: './src/entry/b.js',
+  //   c: './src/entry/c.js',
+  //   d: './src/entry/d.js'
+  // },
   entry: {
-    a: "./src/entry/a.js",
-    b: "./src/entry/b.js",
-    c: "./src/entry/c.js",
-    d: "./src/entry/d.js"
+    main: './src/entry/index.js',
+    vendor: ['lodash']
   },
   output: {
-    filename: "[name].js",
+    filename: '[name].[chunkhash].js',
     path: `${__dirname}/dist` // 输出目录`path`的绝对路径
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
-    new Webpack.optimize.UglifyJsPlugin(),
+    // new Webpack.optimize.UglifyJsPlugin(),
+    new CleanWebpackPlugin(['dist']),
     new ConsoleLogOnBuildWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html',
+      title: 'Caching'
+    }),
+    new Webpack.HashedModuleIdsPlugin(),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'runtime'
     })
   ]
 };
