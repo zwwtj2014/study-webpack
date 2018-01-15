2308b2ae3cad6f8e0382d9350af3dd754ec269e3
zwwtj2014 <529474551@qq.com>
2018-01-16 01:10:06 +0800

    管理资源

--- webpack-guide/src/index.js
+++ webpack-guide/src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Lufei from './lufei.jpg';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 添加图片
+  var lufei = new Image();
+  lufei.src = Lufei;
+  element.appendChild(lufei);
 
   return element;
 }
