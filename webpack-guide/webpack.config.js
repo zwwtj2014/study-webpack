cde0981beccc93862b286c1c14f44bffc488c39b
zwwtj2014 <529474551@qq.com>
2018-01-17 22:56:24 +0800

    缓存：HashedModuleIdsPlugin

--- webpack-guide/webpack.config.js
+++ webpack-guide/webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path');
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
+    vendor: ['lodash']
   },
   output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({ title: 'Prodution' })
+    new HtmlWebpackPlugin({ title: 'Caching' }),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'manifest'
+    })
   ]
 };
